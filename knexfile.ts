ff1988760bb70c75cab4d9672bc5f7f7662ae908
Elias Moreira <121322652+EliasMoreiraDev@users.noreply.github.com>
2024-04-19 08:12:37 -0400

    Add files via upload

--- knexfile.ts
+++ knexfile.ts
@@ -1,12 +1,12 @@
 import path from 'path'
 
 module.exports = {
-    client: 'sqlite30',
+    client: 'sqlite3',
     connection:{
-        filename: path.resolve(__dirname, 'src', 'databgase', 'database.sqlite')
+        filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
     },
     migrations: {
-        diretory: path.resolve(__dirname, 'src', 'databgase', 'migrations')
+        directory: path.resolve(__dirname, 'src', 'database', 'migrations')
     },
     useNullAsDefault:true,
 }
