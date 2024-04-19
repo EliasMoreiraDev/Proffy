782d6583fad07c1dd3cffd33c3cb9cbf88e80a1b
Elias Moreira <121322652+EliasMoreiraDev@users.noreply.github.com>
2024-04-19 11:18:25 -0400

    Add files via upload

--- src/routes.ts
+++ src/routes.ts
@@ -1,28 +1,16 @@
 import express from "express"
-import db from "./database/connection";
+import ClassesController from "./controllers/ClassesController"
+import ConnectionsController from "./controllers/ConnectionsController"
+
 
 const routes = express.Router()
 
-routes.post('/classes', async (request, response) =>{
-    const {
-        name,
-        avatar,
-        whatsapp,
-        bio,
-        subjecy,
-        cost,
-        schedule
-    } = request.body;
+const classesControllers = new ClassesController
+const connectionsController = new ConnectionsController()
 
-    await db('users').insert({
-        name,
-        avatar,
-        whatsapp,
-        bio,
+routes.get('/classes', classesControllers.index)
+routes.post('/classes', classesControllers.create)
 
-    })
-
-    return response.send()
-})
-
+routes.get('/connections', connectionsController.index)
+routes.post('/connections', connectionsController.create)
 export default routes
